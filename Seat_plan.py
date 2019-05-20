8c3043feb8e8235774aabcb1c6955a9f276177e5
ChengChiongWah <ChengChiongWah@users.noreply.github.com>
2019-05-20 12:38:18 +0800

    Add files via upload

--- Seat_plan.py
+++ Seat_plan.py
@@ -7,9 +7,10 @@ data = xlrd.open_workbook("students.xlsx")
 table = data.sheets()[0]
 nrows = table.nrows
 students = []
-for i in range(nrows):
+total_col_numbers = 10
+for i in range(1,nrows):
     print(i, table.row_values(i)[1])
-    students.append(table.row_values(i)[1])    # 把所有的姓名放进列表
+    students.append("  ".join([str(int(table.row_values(i)[0])), table.row_values(i)[1]]))    # 把所有的姓名放进列表
 
 random.shuffle(students)   # 打乱列表中的数据
 #
@@ -20,11 +21,12 @@ random.shuffle(students)   # 打乱列表中的数据
 wbk = xlwt.Workbook()
 sheet = wbk.add_sheet("sheet1")
 
-insert_row_number = 0
+insert_row_number = 1
 col_nuber = 0
+sheet.write(0, 10, "讲台")
 for s in range(len(students)):
     sheet.write(insert_row_number, col_nuber, students[s])
-    if col_nuber + 2 >19:   #19是班级是10排，中间隔一个excel列故是：2*10-1 = 19.
+    if col_nuber + 2 >total_col_numbers*2-1:   #19是班级是10排，中间隔一个excel列故是：2*10-1 = 19.
         col_nuber = 0
         insert_row_number = insert_row_number + 1
     else:
